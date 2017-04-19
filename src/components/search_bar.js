7e08af45c11df6a6bbc980004924a9575faf9d17
Matthew Hurewitz <matthew.hurewitz@bestbuy.com>
2017-04-19 14:27:54 -0500

    created SearchBar component and added it to App

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return <input />; //React.createElement so import react
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAI6LAE6iOZS4QtX_ynBuF9Qnp4QcENKIM'
 
 // Create a new component.  This should produce
 // some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take the component's generated HTML and put it
