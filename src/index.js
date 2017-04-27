22fb5aefc38414a667a57a15d03ef229d48e90ad
Matthew Hurewitz <matthewhurewitz@Matthews-MacBook-Air.local>
2017-04-26 21:35:14 -0500

    updated state using class constructor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-
-// Create a new component. This component should produce
-// some HTML
-
-const App = () => {
-    return <div>Hi!</div>;
-}
-
-// Take this component's generated HTML
-// and put it on the page (in the DOM)
-=======
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAI6LAE6iOZS4QtX_ynBuF9Qnp4QcENKIM'
@@ -28,6 +16,6 @@ const App = () => {
 
 // Take the component's generated HTML and put it
 // on the page (in the DOM)
->>>>>>> cd44dc605d2ea8ec0920b3d42031ecdebb0767b1
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
