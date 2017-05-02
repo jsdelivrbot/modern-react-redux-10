f3027737b59be1056f8c44e7ef14922ff3de19ed
Matthew Hurewitz <matthew.hurewitz@bestbuy.com>
2017-05-02 10:59:44 -0500

    changed App to class component and added YTSearch to the constructor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyAI6LAE6iOZS4QtX_ynBuF9Qnp4QcENKIM'
+
+
 
 // Create a new component.  This should produce
 // some HTML
-const App = () => {
-    return (
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            // this.setState({videos: videos});
+        });
+
+    }
+    render () {
+        return (
         <div>
             <SearchBar />
         </div>
-    );
+        );
+    }
 }
 
 // Take the component's generated HTML and put it
